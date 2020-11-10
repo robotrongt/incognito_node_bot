bbe087b2d446cc96075568b3ad9758d33f51f4ec
Enrico Caruso <robotrongt-gitub-io@enricocaruso.net>
2020-11-11 00:16:22 +0100

    Managed PRV rewards fixed bug2

--- src/models/dbnode.go
+++ src/models/dbnode.go
@@ -486,7 +486,9 @@ func (db *DBnode) UpdateMiningKey(miningkey *MiningKey, callback StatusChangeNot
 			log.Println("UpdateMiningKey error:", err)
 		}
 	}
-	if precLastStatus != miningkey.LastStatus || precPRV != miningkey.LastPRV { //status changed, must notify
+
+	log.Printf("UpdateMiningKey STATUS: %s=% %d=%d\n", precLastStatus, miningkey.LastStatus, precPRV, miningkey.LastPRV)
+	if precLastStatus != miningkey.LastStatus { //status changed, must notify
 		log.Printf("UpdateMiningKey found status change for key %s: from \"%s\" to\" %s\".", miningkey.PubKey, precLastStatus, miningkey.LastStatus)
 		err := callback(miningkey, precLastStatus, precPRV)
 		if err != nil {
